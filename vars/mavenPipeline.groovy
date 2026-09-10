545a22c9b83729bb867b657c9fa538bdecbbc411
JohnsonArockisamy <johnson.arockisamy@kumaran.com>
2026-09-10 18:56:51 +0530

    Add more parameters

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -4,9 +4,29 @@ def call(body) {
     body.delegate = config
     body()
 
-    def jdkVersion = config.jdkVersion ?: 'jdk21'
+    def listOfProfiles = config.profiles ? config.profiles.tokenize(',') : null
+    def nodeJsVersion = config.nodeJsVersion ?: 'nodejs-8.9.4'
+    def jdkVersion = config.jdkVersion ?: 'jdk-21.0.6'
+    def mavenVersion = config.mavenVersion ?: 'maven-3.9.9'
+    def emailRecipients = config.emailRecipients ?: 'johnson.arockisamy@kumaran.com'
+    def attachLogToEmail = config.attachLogToEmail == true
+    def sonar = config.sonar == true
+    def nexus = config.nexus == true
+    if (env.BRANCH_NAME == 'main' || env.BRANCH_NAME == 'master' || env.BRANCH_NAME == 'truck' || env.BRANCH_NAME == 'release.*') {
+        echo "Sonar and Nexus will be executed for main | master | truck | release.*: ${env.BRANCH_NAME}"
+        sonar = true
+        nexus = true
+    }
 
-    echo "Using JDK version: ${jdkVersion}"
+    echo "listOfProfiles: ${listOfProfiles}"
+    echo "Maven version: ${mavenVersion}"
+    echo "Node.js version: ${nodeJsVersion}"
+    echo "JDK version: ${jdkVersion}"
+    echo "Email recipients: ${emailRecipients}"
+    echo "Attach log to email: ${attachLogToEmail}"
+    echo "Sonar: ${sonar}"
+    echo "Nexus: ${nexus}"
+    echo "Env BRANCH_NAME: ${env.BRANCH_NAME}"
 
     pipeline {
         options {
@@ -21,7 +41,7 @@ def call(body) {
         }
 
         stages {
-            stage('Build') {
+            stage('BUILD') {
                 options {
                     skipDefaultCheckout()
                     timeout(time: 30, unit: 'MINUTES')
