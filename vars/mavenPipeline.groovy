a7291af3f71194b8198a4ec9ff4bed49140163f7
JohnsonArockisamy <johnson.arockisamy@kumaran.com>
2026-09-12 11:32:56 +0530

    fix maven repo issue

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -160,7 +160,7 @@ def call(body) {
                             echo "Clearing workspace..."
                             deleteDir()
                         }
-                        // checkout scm                        
+                        checkout scm                        
                     }
 
                     echo "${autoDeploy != null ? "Auto-deploying to ${autoDeploy}..." : "No auto-deploy specified."}"
